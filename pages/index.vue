3e185cc3b6e2874076f8aa2f90a58ef72bde1d07
floookay <developer@floookay.de>
2020-03-26 15:54:39 +0100

    improved statistics table

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,69 @@
 <template>
   <section class="section">
-    <b-table :data="data" :columns="columns"></b-table>
+    <b-table
+      ref="table"
+      :data="data"
+      paginated
+      per-page="25"
+      :opened-detailed="defaultOpenedDetails"
+      detailed
+      detail-key="island"
+      :show-detail-icon="showDetailIcon"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+    >
+      <template slot-scope="props">
+        <b-table-column field="island" label="Island">
+          {{ props.row.island }}
+        </b-table-column>
+
+        <b-table-column field="visits" label="Number of Visits" sortable>
+          {{ props.row.visits }}
+        </b-table-column>
+
+        <b-table-column field="fossils" label="Found Fossils" sortable>
+          {{ props.row.fossils }}
+        </b-table-column>
+
+        <b-table-column field="fossils_per" label="Fossils per Island" sortable>
+          {{ Math.round((props.row.fossils / props.row.visits) * 100) + '%' }}
+        </b-table-column>
+
+        <b-table-column field="bottles" label="Found Bottles" sortable>
+          {{ props.row.bottles }}
+        </b-table-column>
+
+        <b-table-column field="bottles_per" label="Bottles per Island" sortable>
+          {{ Math.round((props.row.bottles / props.row.visits) * 100) + '%' }}
+        </b-table-column>
+
+        <b-table-column field="players" label="Discovered by Players" sortable>
+          {{ props.row.players }}
+        </b-table-column>
+      </template>
+
+      <template slot="detail" slot-scope="props">
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-256x256">
+              <img src="https://buefy.org/static/img/placeholder-128x128.png" />
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>{{ props.row.island }}</strong>
+                <small>what kind of fruits/flowers etc.</small>
+                <br />
+                Short notes about the island, maybe specialties
+              </p>
+            </div>
+          </div>
+        </article>
+      </template>
+    </b-table>
   </section>
 </template>
 
@@ -8,71 +71,30 @@
 export default {
   name: 'HomePage',
   data() {
-    const columns = [
-      {
-        field: 'island',
-        label: 'Island',
-        sortable: true
-      },
-      {
-        field: 'visits',
-        label: 'Number of Visits',
-        numeric: true,
-        sortable: true
-      },
-      {
-        field: 'fossil',
-        label: 'Found Fossils',
-        numeric: true,
-        sortable: true
-      },
-      {
-        field: 'fossil_per',
-        label: 'Fossils per Visit',
-        numeric: true,
-        sortable: true
-      },
-      {
-        field: 'bottle',
-        label: 'Found Bottles',
-        numeric: true,
-        sortable: true
-      },
-      {
-        field: 'bottle_per',
-        label: 'Bottles per Visit',
-        numeric: true,
-        sortable: true
-      }
-    ]
     return {
       data: [
         {
           island: 'Island 0',
-          visits: 14,
-          fossil: 3,
-          fossil_per: 0.1,
-          bottle: 3,
-          bottle_per: 0.4
+          visits: 8,
+          fossils: 3,
+          bottles: 1,
+          players: 2
         },
         {
           island: 'Island 1',
-          visits: 14,
-          fossil: 3,
-          fossil_per: 0.1,
-          bottle: 3,
-          bottle_per: 0.4
+          visits: 4,
+          fossils: 1,
+          bottles: 2,
+          players: 1
         },
         {
           island: 'Island 2',
           visits: 14,
-          fossil: 3,
-          fossil_per: 0.1,
-          bottle: 3,
-          bottle_per: 0.4
+          fossils: 1,
+          bottles: 3,
+          players: 4
         }
-      ],
-      columns
+      ]
     }
   }
 }
