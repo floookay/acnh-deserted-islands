765527696f0876d27f0d2b5d2f560b7866341f68
floookay <developer@floookay.de>
2020-03-28 12:10:24 +0100

    added whitespace

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <!--<div class="navbar-brand">
+    <div>
+        <nav
+            class="navbar header has-shadow is-primary"
+            role="navigation"
+            aria-label="main navigation"
+        >
+            <!--<div class="navbar-brand">
         <a class="navbar-item" href="/">
           <img src="~assets/buefy.png" alt="Buefy" height="28" />
         </a>
@@ -16,46 +16,46 @@
           <span />
         </div>
       </div>-->
-    </nav>
+        </nav>
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
+        <section class="main-content columns">
+            <aside class="column is-2 section">
+                <p class="menu-label is-hidden-touch">
+                    General
+                </p>
+                <ul class="menu-list">
+                    <li v-for="(item, key) of items" :key="key">
+                        <nuxt-link :to="item.to" exact-active-class="is-active">
+                            <b-icon :icon="item.icon" /> {{ item.title }}
+                        </nuxt-link>
+                    </li>
+                </ul>
+            </aside>
 
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
-  </div>
+            <div class="container column is-10">
+                <nuxt />
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      items: [
-        {
-          title: 'Statistics',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Islands',
-          icon: 'lightbulb',
-          to: { name: 'islands' }
+    data() {
+        return {
+            items: [
+                {
+                    title: 'Statistics',
+                    icon: 'ferry',
+                    to: { name: 'index' }
+                },
+                {
+                    title: 'Islands',
+                    icon: 'island',
+                    to: { name: 'islands' }
+                }
+            ]
         }
-      ]
     }
-  }
 }
 </script>
