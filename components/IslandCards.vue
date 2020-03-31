6fae8d1cf924b4e39fa0a36645c81e7b78fabd68
floookay <developer@floookay.de>
2020-03-31 16:08:07 +0200

    added island overview as cards

---
+++ components/IslandCards.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container">
+        <div v-for="island in islands" :key="island[0]" class="card">
+            <div class="card-image">
+                <figure class="image">
+                    <img
+                        :src="
+                            '/acnh-deserted-islands/islands/x512/' +
+                                island[1].img
+                        "
+                        alt="image of island cartography"
+                    />
+                </figure>
+            </div>
+            <div class="card-content">
+                <p class="title is-4">
+                    {{ island[0] }}
+                </p>
+                <div class="">
+                    <div class="cardHeader">Fruits:</div>
+                    {{ island[1].fruits }}
+                </div>
+                <div class="">
+                    <div class="cardHeader">Flowers:</div>
+                    {{ island[1].flowers.toString() }}
+                </div>
+                <div class="">
+                    <div class="cardHeader">Notes:</div>
+                    {{ island[1].notes }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+.container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-gap: 0.5em;
+}
+.card {
+    height: max-content;
+}
+.cardHeader {
+    font-weight: bold;
+    display: inline-block;
+}
+</style>
+<script>
+import islands from '~/assets/islands.json'
+
+export default {
+    data() {
+        return {
+            islands: Object.entries(islands),
+            columns: [
+                { field: 'fruits', label: 'Fruits' },
+                { field: 'flowers', label: 'Flowers' },
+                { field: 'notes', label: 'Notes' }
+            ]
+        }
+    }
+}
+</script>
--- pages/islands.vue
+++ pages/islands.vue
@@ -1,43 +1,27 @@
 <template>
-    <div class="columns is-mobile">
-        <card title="Free" icon="github-circle">
-            Open source on
-            <a href="https://github.com/buefy/buefy">
-                GitHub
-            </a>
-        </card>
-
-        <card title="Responsive" icon="cellphone-link">
-            <b class="has-text-grey">
-                Every
-            </b>
-            component is responsive
-        </card>
-
-        <card title="Modern" icon="alert-decagram">
-            Built with
-            <a href="https://vuejs.org/">
-                Vue.js
-            </a>
-            and
-            <a href="http://bulma.io/">
-                Bulma
-            </a>
-        </card>
-
-        <card title="Lightweight" icon="arrange-bring-to-front">
-            No other internal dependency
-        </card>
-    </div>
+    <section class="section">
+        <IslandCards />
+    </section>
 </template>
+<style scoped>
+.container {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-gap: 0.5em;
+}
+
+.card {
+    height: max-content;
+}
+</style>
 <script>
-import Card from '~/components/Card'
+import IslandCards from '~/components/IslandCards'
 
 export default {
-    name: 'HomePage',
+    name: 'Islands',
 
     components: {
-        Card
+        IslandCards
     }
 }
 </script>
